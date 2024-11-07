6d990df362a0fbf73b86fcea085c10a39e4bb26e
Isaias <cibyest@gmail.com>
2024-11-07 09:51:44 -0600

    First commit

---
+++ src/Components/Extra/Extra.tsx
@@ -0,0 +1,8 @@
+// src/components/ExtraPage.tsx
+import React from 'react';
+
+const ExtraPage: React.FC = () => {
+    return <h1>Esta es la Página Extra</h1>;
+};
+
+export default ExtraPage;
---
+++ src/Routing.tsx
@@ -0,0 +1,16 @@
+// src/Routing.tsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import ExtraPage from "./Components/Extra/Extra";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/extra" element={<ExtraPage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
